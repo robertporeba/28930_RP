b4812f920628ae60921a5cdb260d91f269779b51
Robert Poręba <webberr14@gmail.com>
2021-04-25 11:58:24 +0200

    Add PWIR_07

---
+++ PWIR_07/WCF_RP/App_Code/Usluga1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+// NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Usluga1" in code, svc and config file together.
+public class Usluga1 : IUsluga1
+{
+    public int Dodawanie(int a, int b)
+    {
+        return a + b;
+    }
+
+    public int Mnozenie(int a, int b)
+    {
+        return a * b;
+    }
+}
